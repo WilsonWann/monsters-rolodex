d911c21ffb3a1304b541070c0b206419cbc2907c
DESKTOP-DO30QMK\User <genius19880825@hotmail.com>
2024-03-29 17:04:15 +0800

    building class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
+import { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      monsters: [],
+      searchField: ""
+    }
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase()
+    this.setState({ searchField })
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(monsters => this.setState({ monsters }))
+  }
+
+  render() {
+
+    const { monsters, searchField } = this.state
+    const { onSearchChange } = this
+
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLocaleLowerCase().includes(searchField)
+    )
+
+    return (
+      <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder='search monsters'
+          onChange={onSearchChange}
+        />
+        {
+          filteredMonsters.map((monster) =>
+            <MonsterCard key={monster.id} monster={monster} />)
+        }
+
+      </div>
+    );
+
+  }
 }
 
 export default App;
+class MonsterCard extends Component {
+
+  constructor(props) {
+    super(props)
+    // console.log("🚀 ~ MonsterCard ~ constructor ~ props:", props)
+    this.state = {
+      monster: props.monster,
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.monster.name}</h1>
+      </div>
+    );
+  }
+}
+
